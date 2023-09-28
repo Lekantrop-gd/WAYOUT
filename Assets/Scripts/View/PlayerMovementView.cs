0557f0857269a5490f186245672241701a06f162
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-09-28 10:42:46 +0300

    Tried to design an architecture, but nothing came out

--- Assets/Scripts/View/PlayerMovementView.cs
+++ Assets/Scripts/View/PlayerMovementView.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class PlayerMovementView : MovementView
 {
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
 }
